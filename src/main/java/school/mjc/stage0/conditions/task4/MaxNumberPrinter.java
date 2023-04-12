c2a2a6d49aacc5cc7f0276081a87852c39c257ed
LVLAD2009@yandex.ru <24233lebedko>
2023-04-12 23:43:39 +0300

    stage0-module4-condition-task4

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
-        if (first > second) {
+        if (first >= second) {
             if (first >= third) {
                 System.out.println(first);
             } else {
